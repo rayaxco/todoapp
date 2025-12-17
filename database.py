3c06ea7c30de64458c380ca3b0886512a1c40923
rayaxco <rayax912@gmail.com>
2025-12-17 14:52:24 +0000

    deleted todosapp.db and removed mysql connection

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL='sqlite:///./todoapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False},poolclass=StaticPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
